c693ce1403e2bdeca10b166b45916e734a10adfa
hhp227 <hong227@naver.com>
2023-05-06 14:00:03 +0900

    2023.05.06 Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿//
+//  PlayerController.cs
+//  Gunkata
+//
+//  Created by hhp227 on 2023/05/05.
+//  Copyright © 2023 ?????. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+    Animator animator;
+    Rigidbody2D rigidbody2D;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        animator.SetBool("run", false);
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Translate(Vector2.right * 0.005f);
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool("run", true);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Translate(Vector2.left * 0.005f);
+            transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetBool("run", true);
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidbody2D.AddForce(Vector2.up * 4);
+            rigidbody2D.gravityScale = 2;
+        }
+    }
+}
