4318ff5cfe7985d368f250313357ba36cb1f02ed
whygunkata <whyvwpcw@gmail.com>
2023-05-14 19:10:49 +0900

    2023.05.14b

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,10 +14,12 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour {
     Animator animator;
     Rigidbody2D rigidbody2D;
+    public static int bulletdir = 0;
 
     void Start() {
         animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
+        int bulletdir = 0;
     }
 
     //주석테스트
@@ -27,16 +29,23 @@ public class PlayerController : MonoBehaviour {
             transform.Translate(Vector2.right * 0.005f);
             transform.localScale = new Vector3(1, 1, 1);
             animator.SetBool("run", true);
+            bulletdir = 0;
         }
         if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.Translate(Vector2.left * 0.005f);
             transform.localScale = new Vector3(-1, 1, 1);
             animator.SetBool("run", true);
+		bulletdir = 1;
+
         }
         if (Input.GetKey(KeyCode.Space)) {
             rigidbody2D.AddForce(Vector2.up * 4);
             rigidbody2D.gravityScale = 2;
         }
 
+    }
+
+    void Print() {
+	  Debug.Log(transform.position);
     }
 }
